2520d4ef8b9eafb6511bf893f080082e06cf2434
Владимир <melnikoffspb@ya.ru>
2023-10-19 10:25:06 +0300

    final step

--- src/main/java/hexlet/code/config/RollbarConfig.java
+++ src/main/java/hexlet/code/config/RollbarConfig.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @Configuration
+@EnableWebMvc
 @ComponentScan({
     "hexlet.code"
 })
